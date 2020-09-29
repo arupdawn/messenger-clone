53511a48cc1e7ca5512a996902f8f005ee3c4068
Arup Chandra Dawn <arupchandradawn@gmail.com>
2020-09-29 05:30:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,6 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxRb4j-kqhXDzT5mQqbaCJgQ81MCTrN6c",
-  authDomain: "messenger-clone-ca485.firebaseapp.com",
-  databaseURL: "https://messenger-clone-ca485.firebaseio.com",
-  projectId: "messenger-clone-ca485",
-  storageBucket: "messenger-clone-ca485.appspot.com",
-  messagingSenderId: "1061262030489",
-  appId: "1:1061262030489:web:99056a9f6081ee2ff0aa08",
-  measurementId: "G-DN6SJKDMEY",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
